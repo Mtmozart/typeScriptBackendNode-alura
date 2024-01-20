3d45a29bccd23bafef5fe67c999602a59dab9643
matheus mozart <mmsnborges@gmail.com>
2024-01-19 21:04:34 -0400

    relação one to one

--- src/entities/AdotanteEntity.ts
+++ src/entities/AdotanteEntity.ts
@@ -1,4 +1,5 @@
-import { Column, DefaultNamingStrategy, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, DefaultNamingStrategy, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import EnderecoEntity from "./EnderecoEntity";
 
 @Entity()
 export default class AdotanteEntity{
@@ -12,11 +13,12 @@ export default class AdotanteEntity{
   celular: string;
   @Column({nullable:true})
   foto?: string;
-  @Column({nullable:true})
-  endereco?: string;
+  @OneToOne(() => EnderecoEntity,{nullable: true, cascade: true, eager: true })
+  @JoinColumn()//indica um campo estrangeiro para o adotante
+  endereco?: EnderecoEntity;
 
 
-  constructor(nome: string, senha: string, celular: string, foto?:string, endereco?: string){
+  constructor(nome: string, senha: string, celular: string, foto?:string, endereco?: EnderecoEntity){
     this.nome = nome,
     this.senha = senha;
     this.celular = celular,
